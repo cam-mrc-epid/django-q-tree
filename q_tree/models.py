9b0ff4a13abaf9ba24c246fc3cb0d1fd5bd69486
davidgillies <david.gillies.rhu@gmail.com>
2015-10-20 15:34:26 +0100

    Update models.py

--- q_tree/models.py
+++ q_tree/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from polymorphic_tree.models import PolymorphicMPTTModel, PolymorphicTreeForeignKey
+from polymorphic_tree.managers import PolymorphicMPTTModelManager
+from model_utils.managers import InheritanceManager
 
 
 class BaseTreeNode(PolymorphicMPTTModel):
     parent = PolymorphicTreeForeignKey('self', blank=True, null=True, related_name='children', verbose_name=_('parent'))
     title = models.CharField(_("Title"), max_length=200)
+    objects = PolymorphicMPTTModelManager()
+    copy_objects = InheritanceManager()
 
     def __unicode__(self):
         return self.title
